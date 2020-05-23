6c89c3ed69cbe3fcf49871e23aae544f000bb3b2
Beryl01 <berylnegesa@gmail.com>
2020-05-24 01:23:12 +0300

    Created first database

--- pics/models.py
+++ pics/models.py
@@ -1,4 +1,17 @@
+from __future__ import unicode_literals
+from django.db import models
 
+# Create your models here.
+ 
+class Image(models.Model):
+    Name = models.CharField(max_length=60)
+    description = models.TextField(default=Deafult_desc)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/' ,default='default.jpg')
+
+    def __str__(self):
+        return self.Name  
   
     
 
