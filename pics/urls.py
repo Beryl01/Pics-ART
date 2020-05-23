946b3dba887495355661feae161f3ab044f23acf
Beryl01 <berylnegesa@gmail.com>
2020-05-24 01:39:39 +0300

    Created base html and gallery page to view all photos updated

--- pics/urls.py
+++ pics/urls.py
@@ -1,7 +1,11 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('gallery/', views.gallery, name='gallery'),
 
 ]
 
--- pics/views.py
+++ pics/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse, Http404
+from .models import Image, Location, Category
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-    
+def gallery(request):
+    image = Image.objects.all()
+    return render(request, 'images.html', {"image": image}) 
 
 
 
