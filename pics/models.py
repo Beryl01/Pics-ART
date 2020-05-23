41ce76c8bffb3a35d02f907fc8f9950e8f1b80e6
Beryl01 <berylnegesa@gmail.com>
2020-05-24 01:27:51 +0300

    Made migrations for  the complete  database

--- pics/models.py
+++ pics/models.py
@@ -18,6 +18,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.Name        
+
+class Location(models.Model):
+    place = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.place        
   
     
 
