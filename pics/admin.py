2b17971198ea8eac0328f972f2dc93802bf2f227
Beryl01 <berylnegesa@gmail.com>
2020-05-24 01:35:30 +0300

    REgistered models in db to Admin page and dashboard

--- pics/admin.py
+++ pics/admin.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
+
 # Register your models here.
 from django.contrib import admin
-from .models import Image
+from .models import Category, Location, Image
 
+admin.site.register(Category)
+admin.site.register(Location)
 admin.site.register(Image)
-
